1b73c1e656a2838d4b984d9fbecbd49c8d3cbcdd
Axel Arcidiaco <axel.arcidiaco@gmail.com>
2025-04-07 16:13:54 +0200

    game loop
    Ajout de la boucle while du jeu et de la boucle if, elif, else qui permet à l'utilisateur de savoir si le nombre qu'il donne est plus grand ou plus petit que le nombre à trouvé et s'il a gagné

--- number_guess_game.py
+++ number_guess_game.py
@@ -14,10 +14,21 @@ victoire = False
 # Compteur d'essai de l'utilisateur
 cpt_essai = 0
 
-# 
+# Messages d'introduction
 print("=========================== Trouve le nombre ===========================")
 print("Le but de ce jeu est de deviner un nombre aléatoire entre 0 et 1000.")
 
-# # Boucle du jeu
-# while (victoire == False):
-#     nombre_utilisateur = int(input("Donne-moi un entier positif : "))
+# Boucle du jeu
+while (victoire == False):
+    nombre_utilisateur = int(input("Donne-moi un entier positif : "))
+    if (nombre_utilisateur < nombre_a_deviner):
+        print("Le nombre a deviné est plus grand.")
+        cpt_essai += 1
+    elif (nombre_utilisateur > nombre_a_deviner):
+        print("Le nombre a deviné est plus petit.")
+        cpt_essai += 1
+    else:
+        print("Bravo, vous avez trouvé le nombre à deviner.")
+        print("Le nombre a deviné était ",nombre_a_deviner)
+        print("Vous l'avez trouvé en", cpt_essai,"essai(s)")
+        victoire = True
